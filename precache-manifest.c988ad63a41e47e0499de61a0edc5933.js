430b9879b18ea3887235c523588262ae3e5bdc1d
Scott Dickerson <swiftfoot@hotmail.com>
2019-01-17 17:57:53 -0600

    Updates

---
+++ precache-manifest.c988ad63a41e47e0499de61a0edc5933.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "86c12397eb20a0948b1f",
+    "url": "/spying-with-stem/static/css/main.e8634dec.chunk.css"
+  },
+  {
+    "revision": "86c12397eb20a0948b1f",
+    "url": "/spying-with-stem/static/js/main.86c12397.chunk.js"
+  },
+  {
+    "revision": "bdd67ffb86083b3952ac",
+    "url": "/spying-with-stem/static/css/1.72b1404f.chunk.css"
+  },
+  {
+    "revision": "bdd67ffb86083b3952ac",
+    "url": "/spying-with-stem/static/js/1.bdd67ffb.chunk.js"
+  },
+  {
+    "revision": "692f1ab26b6f9cb4a022",
+    "url": "/spying-with-stem/static/js/runtime~main.692f1ab2.js"
+  },
+  {
+    "revision": "abf254e0d01cadf6a680736d2ffaed9a",
+    "url": "/spying-with-stem/static/media/double_pop.abf254e0.mp3"
+  },
+  {
+    "revision": "958e71a835d655a9757e232f205b3375",
+    "url": "/spying-with-stem/static/media/envelope.958e71a8.mp3"
+  },
+  {
+    "revision": "a6649a4dc290519258ad104b6d4a2791",
+    "url": "/spying-with-stem/static/media/pistol.a6649a4d.mp3"
+  },
+  {
+    "revision": "a233cdb35e0a0bda2c46588209424d16",
+    "url": "/spying-with-stem/static/media/Background.a233cdb3.png"
+  },
+  {
+    "revision": "d1e9108c6b11ac38a02c4ff9e31bc49e",
+    "url": "/spying-with-stem/static/media/swoosh_slow.d1e9108c.mp3"
+  },
+  {
+    "revision": "3f406320fb893df9830a7c536a552ffa",
+    "url": "/spying-with-stem/static/media/Typewriter_short.3f406320.mp3"
+  },
+  {
+    "revision": "11b49f61e6131a0aa43c4897d980d313",
+    "url": "/spying-with-stem/static/media/typewriter_ding.11b49f61.mp3"
+  },
+  {
+    "revision": "de891092e1dd06c7518dc85e7db5667f",
+    "url": "/spying-with-stem/static/media/stamp.de891092.mp3"
+  },
+  {
+    "revision": "75ea5448c8d49f008736adc3e2f3f488",
+    "url": "/spying-with-stem/static/media/win_tone.75ea5448.mp3"
+  },
+  {
+    "revision": "d641ab89bc1792321a49cd401919b276",
+    "url": "/spying-with-stem/static/media/button_chime.d641ab89.mp3"
+  },
+  {
+    "revision": "a9676f810d477bf0591b08c8919840c5",
+    "url": "/spying-with-stem/static/media/paper_write_long.a9676f81.mp3"
+  },
+  {
+    "revision": "d08576ec5dbf32d48374d817f5440fcd",
+    "url": "/spying-with-stem/static/media/franklingothic-book.d08576ec.otf"
+  },
+  {
+    "revision": "bd513477436f5753bbe770da1ca1544c",
+    "url": "/spying-with-stem/static/media/LiberationSans-Regular.bd513477.ttf"
+  },
+  {
+    "revision": "45465ceb1350a260a06bacf46c6d5e5c",
+    "url": "/spying-with-stem/static/media/Ghostlight-Regular.45465ceb.ttf"
+  },
+  {
+    "revision": "daf7e1cfa7752d93132a35ed57255010",
+    "url": "/spying-with-stem/static/media/CodeBook.daf7e1cf.png"
+  },
+  {
+    "revision": "a67c1189452c2fb22e556c71142ec8ac",
+    "url": "/spying-with-stem/static/media/Computing.a67c1189.png"
+  },
+  {
+    "revision": "61c2d6a78975583d4ae9775add6794cb",
+    "url": "/spying-with-stem/static/media/Cypher.61c2d6a7.png"
+  },
+  {
+    "revision": "2b470473f7579cabdb02bcaffda37ff0",
+    "url": "/spying-with-stem/static/media/airy_stick.2b470473.mp3"
+  },
+  {
+    "revision": "6c275dd4a4a203851b4ccad6c52926cc",
+    "url": "/spying-with-stem/static/media/paper_write_short.6c275dd4.mp3"
+  },
+  {
+    "revision": "1b69cce5cda6884a61b00df4709b4606",
+    "url": "/spying-with-stem/static/media/Artboard_1.1b69cce5.png"
+  },
+  {
+    "revision": "27d458ac77df22b1bcb5d84a489c06e7",
+    "url": "/spying-with-stem/static/media/broken.27d458ac.png"
+  },
+  {
+    "revision": "4a58a3e8acfc7f4be47c2b776c45c9d8",
+    "url": "/spying-with-stem/static/media/United_Daughters_of_the_Confederacy-BW.4a58a3e8.jpg"
+  },
+  {
+    "revision": "af452309bae9797709e4065d9b88ea8c",
+    "url": "/spying-with-stem/static/media/telegraph_short.af452309.mp3"
+  },
+  {
+    "revision": "39f88597f2b91b293f112e9bd86796e8",
+    "url": "/spying-with-stem/static/media/slide.39f88597.mp3"
+  },
+  {
+    "revision": "b7cb1b633bfaa0c8005d18f9edbe9fdc",
+    "url": "/spying-with-stem/static/media/multiSwoosh.b7cb1b63.mp3"
+  },
+  {
+    "revision": "f44cf8494a2f88e5fb175c4fbc492519",
+    "url": "/spying-with-stem/static/media/broken.f44cf849.png"
+  },
+  {
+    "revision": "68220cab887e858d3d2f0fc58c5e46b3",
+    "url": "/spying-with-stem/static/media/intercepted.68220cab.png"
+  },
+  {
+    "revision": "a82662f9b570434e36b7354bce008cce",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00000.a82662f9.png"
+  },
+  {
+    "revision": "bcebaf1fab2299235221f714920c51f7",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00001.bcebaf1f.png"
+  },
+  {
+    "revision": "53aa9f61c300c6d4ed05b7f9f5c59c7b",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00002.53aa9f61.png"
+  },
+  {
+    "revision": "7510dd79894be3db3370846181dab70b",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00003.7510dd79.png"
+  },
+  {
+    "revision": "507fa0220cb3ada212d245625c0a48d4",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00004.507fa022.png"
+  },
+  {
+    "revision": "d1b8b3b0db2eabe8a8465ef2cce43986",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00005.d1b8b3b0.png"
+  },
+  {
+    "revision": "526cbb260dee83a10bef3e80e925bf71",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00006.526cbb26.png"
+  },
+  {
+    "revision": "3a81b1918a9f9005555f517d0eeae105",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00007.3a81b191.png"
+  },
+  {
+    "revision": "876cce68de619771255d0a23427b3db1",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00008.876cce68.png"
+  },
+  {
+    "revision": "4314b4bdc68fdd6ba7068425a3014e2b",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00009.4314b4bd.png"
+  },
+  {
+    "revision": "e724daac679443b3044acfae99d87b1d",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00010.e724daac.png"
+  },
+  {
+    "revision": "c2a3cb978a53eb75502b3e3bf5c44686",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00011.c2a3cb97.png"
+  },
+  {
+    "revision": "a690e4d3a2b14911252dee978ebff7a1",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00012.a690e4d3.png"
+  },
+  {
+    "revision": "33cdb8e793fca2e886d59a4155f897f3",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00013.33cdb8e7.png"
+  },
+  {
+    "revision": "78f36342561b825b3dcf26fc8ee48aef",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00014.78f36342.png"
+  },
+  {
+    "revision": "a0772130a7d04aab217f7144b45bc37f",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00015.a0772130.png"
+  },
+  {
+    "revision": "264a75d7d1212fab8485878422fa20a3",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00016.264a75d7.png"
+  },
+  {
+    "revision": "cd524788c4a48b10258c86dc9056273b",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00017.cd524788.png"
+  },
+  {
+    "revision": "4f4fcbd3ecac90e218584403e12fb386",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00018.4f4fcbd3.png"
+  },
+  {
+    "revision": "52e22c7f4986472f60bfc861d865955d",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00019.52e22c7f.png"
+  },
+  {
+    "revision": "6927126eba87f6046f592901446a8fca",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00020.6927126e.png"
+  },
+  {
+    "revision": "efacdd438ee418d8a852cec08e82ee91",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00021.efacdd43.png"
+  },
+  {
+    "revision": "06d35fceb2471b8d20cd2881d1e781c5",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00022.06d35fce.png"
+  },
+  {
+    "revision": "956d3ddd124b7238893a909f9afb754b",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00023.956d3ddd.png"
+  },
+  {
+    "revision": "0df4db3baf9a15feeb651781e9044aa9",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00024.0df4db3b.png"
+  },
+  {
+    "revision": "4909d699169e60cf2e022200cedc2313",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00025.4909d699.png"
+  },
+  {
+    "revision": "4ea96e54dca44771f48f53268158805a",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00026.4ea96e54.png"
+  },
+  {
+    "revision": "43ce80055572a02ae55ca2d206d322af",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00027.43ce8005.png"
+  },
+  {
+    "revision": "1d75d3865cd0596406c4dc314a0f64c3",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00028.1d75d386.png"
+  },
+  {
+    "revision": "4224ddfc5360106e39ac16e21b0ffe18",
+    "url": "/spying-with-stem/static/media/codebook_sprite_00029.4224ddfc.png"
+  },
+  {
+    "revision": "f52e3087f75752eecf9a2a4564e1d0f7",
+    "url": "/spying-with-stem/index.html"
+  }
+];
